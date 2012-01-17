a79827615265a6de81fd5083c506ecd0f11782d7
AJ Christensen <aj@junglist.gen.nz>
2012-01-17 13:40:45 +1300

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+maintainer       "AJ Christensen"
+maintainer_email "aj@junglist.gen.nz"
+license          "Apache 2.0"
+description      "Configures the pennyworth continuous deployment pipeline system"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.2.2"
+
+depends "java"
+depends "jenkins"
+depends "postfix"
+depends "build-essential"
+depends "reprepro"
+depends "leiningen"
+depends "ant"
